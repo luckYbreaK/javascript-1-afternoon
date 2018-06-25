2004ae13dd212f4656394a79a3a39b23626f5eed
Gregory Hart <gskhrvat@yahoo.com>
2018-06-25 13:56:21 -0600

    First Commit

--- practice.js
+++ practice.js
@@ -5,28 +5,32 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting(name) {
+  return "Hello, " + name;
+}
 
 //////////////////PROBLEM 2////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
+var newGreeting = function(name) {
+  return "Hello, " + name;
+}
 
 //////////////////PROBLEM 3////////////////////
 
 //Rewrite the function greeting as an arrow function.
 //Name it finalGreeting.
 
-//Code Here
+var finalGreeting = name => "Hello, " + name; 
 
 //////////////////PROBLEM 4////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+var groceries = ['apples', 'milk','eggs', 'bread'];
 
 //Write a function called doubleCheck that takes in an array
 //as a parameter.
@@ -34,7 +38,12 @@
 //If the array does not contain "chocolate", add "chocolate".
 //doubleCheck should return the array.
 
-//Code Here
+function doubleCheck(array) {
+  if(!array.includes("chocolate")) {
+    array.push("chocolate");
+  }
+  return array;
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -43,7 +52,12 @@
 //name (a string), color: (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+var dog = {
+  name: "Buttons",
+  color: "White",
+  age: 8,
+  goodBoy: true
+};
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
